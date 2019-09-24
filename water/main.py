79b7f0b77dac53392a74883119e6b75100466f58
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-09-24 15:53:41 +0100

    pollution used for node colour

--- water/main.py
+++ water/main.py
@@ -3,6 +3,7 @@ from bokeh.layouts import layout
 from bokeh.models.graphs import from_networkx, NodesAndLinkedEdges
 from bokeh.models import Range1d, Plot, MultiLine, Circle, HoverTool, Slider, Button
 from bokeh.palettes import Spectral4
+from bokeh.transform import linear_cmap
 import pickle
 import wntr
 from os.path import dirname, join
@@ -22,6 +23,10 @@ filename = join(dirname(__file__), 'data', 'kentucky_water_distribution_networks
 with open(filename, 'rb') as input_file:
     pollution = pickle.load(input_file)
 
+# Use 700th timestep of pollution starting at J-10 as an example
+pollution_series = pollution['J-10'].iloc[700]
+pollution_values = []
+
 slider = Slider(start=0, end=3, value=0, step=1, title="Time")
 button = Button(label='► Play', width=60)
 
@@ -34,6 +39,8 @@ for node_name, node_data in nodes_dict.items():
 
     x.append(node_data['pos'][0])
     y.append(node_data['pos'][1])
+
+    pollution_values.append(pollution_series[node_name])
 
 x = [(i - min(x)) / (max(x) - min(x)) for i in x]
 y = [(i - min(y)) / (max(y) - min(y)) for i in y]
@@ -48,11 +55,13 @@ graph = from_networkx(G, locations)
 plot = Plot(x_range=Range1d(-0.1, 1.1), y_range=Range1d(-0.1, 1.1))
 
 # Create nodes and edges
-graph.node_renderer.glyph = Circle(size=2, fill_color=Spectral4[0])
+graph.node_renderer.data_source.data['colors'] = pollution_values
+# TODO: set a constant value for the max of the color range
+graph.node_renderer.glyph = Circle(size=5, fill_color=linear_cmap('colors', 'Spectral8', min(pollution_values), max(pollution_values)))
 graph.edge_renderer.glyph = MultiLine(line_alpha=1.6, line_width=0.5)
 
 # green hover for both nodes and edges
-graph.node_renderer.hover_glyph = Circle(size=2, fill_color='#abdda4')
+graph.node_renderer.hover_glyph = Circle(size=5, fill_color='#abdda4')
 graph.edge_renderer.hover_glyph = MultiLine(line_color='#abdda4', line_width=1)
 
 # When we hover over nodes, highlight adjecent edges too
