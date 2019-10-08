d1df09c53c31452e8079b9841ca55bbb77829f8c
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-10-08 15:35:07 +0100

    fix test assertions

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,12 +3,12 @@ from water.main import get_node_sizes
 
 
 def test_get_pollution_values():
-    assert(get_pollution_values('J-1', 42330)[0], 6.503379)
+    assert float(get_pollution_values('J-1', 42330)[0]) == 6.503378868103027
 
 
 def test_get_pollution_values_timestep_doesnt_exist():
-    assert(get_pollution_values('J-100', 14)[0], 0.0)
+    assert get_pollution_values('J-100', 14)[0] == 0.0
 
 
-# def test_get_node_sizes():
-#     assert(get_node_sizes())
+def test_get_pollution_values_node_doesnt_exist():
+    assert get_pollution_values('X', 42330)[0] == 0.0
