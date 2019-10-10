3b7e8d7b1c72c12a50766347b599421bb2db8f84
Jim Madge <jmmadge@gmail.com>
2019-10-10 13:46:22 +0100

    Move loading water network to a function

--- water/main.py
+++ water/main.py
@@ -119,51 +119,59 @@ def update_speed(attr, old, new):
                                                      animation_speed)
 
 
-# load .inp file
-filename = join(dirname(__file__), 'data',
-                'kentucky_water_distribution_networks/ky2.inp')
+def load_water_network():
+    # load .inp file
+    filename = join(dirname(__file__), 'data',
+                    'kentucky_water_distribution_networks/ky2.inp')
 
-# Create water network
-wn = wntr.network.WaterNetworkModel(filename)
+    # Create water network
+    wn = wntr.network.WaterNetworkModel(filename)
 
-# Get the NetworkX graph
-G = wn.get_graph().to_undirected()
+    # Get the NetworkX graph
+    G = wn.get_graph().to_undirected()
 
-# Add the node name as an attribute, so we can use with tooltips
-# Also add info about the demand and elevation
-# Also add the names of connected nodes and edge names
-# Also get a list of the base demand for each node
-all_base_demands = []
-for node in G.nodes():
-    G.node[node]['name'] = node
-    try:
-        G.node[node]['elevation'] = wn.query_node_attribute('elevation')[node]
-    except KeyError:
-        G.node[node]['elevation'] = 'N/A'
-    try:
-        base_demands = []
-        # TODO: For some reason this is a list, but in Ky2 data there is only
-        # ever a single base demand value
-        for timeseries in wn.get_node(node).demand_timeseries_list:
-            base_demands.append(timeseries.base_value)
-        base_demand = mean(base_demands)
-        G.node[node]['demand'] = base_demand
-        all_base_demands.append(base_demand)
-    except AttributeError:
-        # Nodes with no demand will not resize from the base_node_size
-        all_base_demands.append(0.0)
-        G.node[node]['demand'] = "N/A"
-    pipes = dict(G.adj[node])
-    connected_str = ""
-    i = 0
-    for connected_node, pipe_info in pipes.items():
-        if i > 0:
-            connected_str = connected_str + "| "
-        connected_str = connected_str + connected_node + ": "
-        for pipe, info in pipe_info.items():
-            connected_str = connected_str + pipe + " "
-        i += 1
-    G.node[node]['connected'] = connected_str
+    # Add the node name as an attribute, so we can use with tooltips
+    # Also add info about the demand and elevation
+    # Also add the names of connected nodes and edge names
+    # Also get a list of the base demand for each node
+    all_base_demands = []
+    for node in G.nodes():
+        G.node[node]['name'] = node
+        try:
+            G.node[node]['elevation'] = (
+                wn.query_node_attribute('elevation')[node]
+                )
+        except KeyError:
+            G.node[node]['elevation'] = 'N/A'
+        try:
+            base_demands = []
+            # TODO: For some reason this is a list, but in Ky2 data there is
+            # only ever a single base demand value
+            for timeseries in wn.get_node(node).demand_timeseries_list:
+                base_demands.append(timeseries.base_value)
+            base_demand = mean(base_demands)
+            G.node[node]['demand'] = base_demand
+            all_base_demands.append(base_demand)
+        except AttributeError:
+            # Nodes with no demand will not resize from the base_node_size
+            all_base_demands.append(0.0)
+            G.node[node]['demand'] = "N/A"
+        pipes = dict(G.adj[node])
+        connected_str = ""
+        i = 0
+        for connected_node, pipe_info in pipes.items():
+            if i > 0:
+                connected_str = connected_str + "| "
+            connected_str = connected_str + connected_node + ": "
+            for pipe, info in pipe_info.items():
+                connected_str = connected_str + pipe + " "
+            i += 1
+        G.node[node]['connected'] = connected_str
+
+    return G, all_base_demands
+
+
+G, all_base_demands = load_water_network()
 
 # This global variable list is used for node resizing and the demand data is
 # also displayed in the tooltip
