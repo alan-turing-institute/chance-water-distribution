e6561e95977a53068df9b1c2d7de1a824ec03628
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-10-30 15:48:32 +0000

    move node type dropdown to select

--- water/modules/html_formatter.py
+++ water/modules/html_formatter.py
@@ -9,6 +9,8 @@ def timer_html(timestep):
 
 
 def pollution_history_html(history_node="None", highlight_color="black"):
+    if history_node == "None":
+        highlight_color = "black"
     pollution_history_html = "<p>Selection: <b style='color:"
     pollution_history_html += highlight_color + "'>"
     pollution_history_html += history_node
@@ -22,3 +24,13 @@ def pollution_loaction_html(injection_node, injection_color):
     pollution_location_html += injection_node
     pollution_location_html += "</b></p>"
     return pollution_location_html
+
+
+def node_type_html(type="None", color="black"):
+    if type == "None":
+        color = "black"
+    node_type_html = "<p>Selection: <b style='color:"
+    node_type_html += color + "'>"
+    node_type_html += type
+    node_type_html += "</b></p>"
+    return node_type_html
