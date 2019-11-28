e28b95dad9b05fac6ecf6099b5fe338cfa74c9eb
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-11-28 14:41:49 +0000

    set ky2 as default or first custom network

--- water/main.py
+++ water/main.py
@@ -21,7 +21,7 @@ from flask import Flask, render_template, request
 from modules.html_formatter import (timer_html, pollution_history_html,
                                     pollution_location_html, node_type_html)
 from modules.load_data import (load_water_network, load_pollution_dynamics,
-                               get_networks)
+                               get_networks, get_custom_networks)
 from modules.pollution import (pollution_series, pollution_history,
                                pollution_scenario)
 from threading import Thread
@@ -50,6 +50,14 @@ def bkapp(doc):
 
     # Color of selected node type
     type_highlight_color = "purple"
+
+    # By default, we want example network ky2 to load into the bokeh app
+    # If however any custom networks are present, that a user has added
+    # make one of these the default selected network
+    default_network = 'ky2'
+    custom_networks = get_custom_networks()
+    if len(custom_networks) > 0:
+        default_network = custom_networks[0]
 
     def update_highlights():
         """Set the color and width for each node and edge in the graph."""
@@ -297,10 +305,10 @@ def bkapp(doc):
         network = args.get('network')[0].decode('utf8')
         if network not in networks:
             # If an invalid network is selected fall back to the default
-            network = networks[0]
+            network = default_network
     else:
         # If there is no request fall back to the deault
-        network = networks[0]
+        network = default_network
 
     G, locations, all_base_demands, include_map = load_water_network(network)
 
