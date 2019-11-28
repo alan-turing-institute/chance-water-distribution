a5c63018898cde67f176a6bdfc82209457f59a41
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-11-28 15:47:02 +0000

    add test to check all nodes in custom network have pollution values

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -8,9 +8,22 @@ def test_get_network_examples():
     assert get_network_examples() == ['ky14', 'ky2', 'ky4', 'ky8', 'ky9']
 
 
-def test_get_custom_networks():
-    """By default, there should be no custom networks"""
-    assert get_custom_networks() == []
+if len(get_custom_networks()) > 0:
+    @pytest.mark.parametrize('network', get_custom_networks())
+    def test_get_custom_networks(network):
+        """By default, there should be no custom networks. If there are, check
+        that there is a pollution file present for every node in the network,
+        with the exception of tanks and reservoirs"""
+        _, injection_nodes, *_ = load_pollution_dynamics(network)
+        networkx_graph, *_ = load_water_network(network)
+        graph_nodes = []
+        for node in networkx_graph.nodes():
+            type = networkx_graph.nodes[node]['type']
+            if type != 'Tank' and type != 'Reservoir':
+                graph_nodes.append(node)
+        injection_nodes.sort()
+        graph_nodes.sort()
+        assert injection_nodes == graph_nodes
 
 
 def test_get_network_files_path_bad_network():
